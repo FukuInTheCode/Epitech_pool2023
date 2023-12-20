0b3037e95dc2e8655c2c8b747f42a32a20dfabef
Tom C. <tom.carlier@epitech.eu>
2023-12-20 11:40:52 +0100

    [~] Update.

---
+++ Day3/my.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 03
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+
+    #include <unistd.h>
+
+    #define MAX_INTEGER 2147483647
+    #define MIN_INTEGER -2147483648
+
+void my_putchar(char c);
+int my_put_nbr(int nb);
+void my_printstr(char *str);
+int is_end_combn(int nb, int i);
+#endif
---
+++ Day3/tests/tests_my_put_char.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 04
+** File description:
+** Placeholder
+*/
+
+#include "../my.h"
+
+int main(void)
+{
+	my_put_nbr(1);
+	my_put_nbr(-1);
+	my_put_nbr(MAX_INTEGER);
+	my_put_nbr(MIN_INTEGER);
+	my_put_nbr(0);
+	return 0;
+}
