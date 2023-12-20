8c63cfffd749f1b51e513eaa34aaf6ae0aa168cb
Tom C. <tom.carlier@epitech.eu>
2023-12-20 11:38:36 +0100

    [+] Add my_isneg.

---
+++ my_isneg.c
@@ -0,0 +1,5 @@
+int my_isneg(int n)
+{
+	my_putchar('P' * (n >= 0) + 'N' * (n < 0));
+	return 0;
+}
---
+++ my_print_digits.c
@@ -0,0 +1,9 @@
+
+void my_putchar(char c);
+
+int my_print_digits(void)
+{
+	for (short i = 48; i < 58; i++)
+		my_putchar(i);
+	return 0;
+}
