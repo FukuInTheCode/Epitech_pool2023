cafb0441f0f34a537c92e7e3152beea21754647f
Tom C. <tom.carlier@epitech.eu>
2023-12-20 11:38:59 +0100

    [~] Redo the my_print_comb2.c

--- my_print_comb2.c
+++ my_print_comb2.c
@@ -1,21 +1,26 @@
 
 #include "my.h"
 
+static void my_print_second(char i, char j)
+{
+    for (char k = i; k <= '9'; k++) {
+        for (char l = j; l <= '9'; l++) {
+            my_putchar(i);
+            my_putchar(j);
+            my_putchar(' ');
+            my_putchar(k);
+            my_putchar(l);
+        }
+    }
+}
+
 int my_print_comb2(void)
 {
     for (char i = '0'; i <= '9'; i++) {
         for (char j = '0'; j <= '9'; j++) {
-            for (char k = i; k <= '9'; k++) {
-                for  (char l = j + 1; l <= '9'; l++) {
-                    my_putchar(i);
-                    my_putchar(j);
-                    my_putchar(' ');
-                    my_putchar(k);
-                    my_putchar(l);
-                    my_putchar(',');
-                    my_putchar(' ');
-                }
-            }
+            my_print_second(i, j);
         }
     }
+    my_putchar('\b');
+    my_putchar('\b');
 }
