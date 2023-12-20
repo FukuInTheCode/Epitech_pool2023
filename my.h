e21ec4de5d5704aa126372334cd59df6ec218f4a
Tom C. <tom.carlier@epitech.eu>
2023-12-20 11:42:35 +0100

    [~] Bug fix my_swap.c

---
+++ my.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 04
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #include <unistd.h>
+void my_putchar(char c);
+int my_put_nbr(int nb);
+#endif
