ef7aae400101f15af38dcabeb62878e468ec2919
Tom C. <tom.carlier@epitech.eu>
2023-12-20 11:38:39 +0100

    [+] Add my_print_comb2.c

---
+++ my_print_comb2.c
@@ -0,0 +1,18 @@
+
+void my_putchar(char c);
+
+static void my_print(char n)
+{
+	for (char i = n + 1; i <= '9'; i++) {
+		my_putchar(n);
+		my_putchar(i);
+		my_putchar(' ');
+	}
+}
+
+int my_print_comb2(void)
+{
+	for (char i = '0'; i <= '8'; i++)
+		my_print(i);
+	return 0;
+}
