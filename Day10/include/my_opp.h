63517dc79c30d7b51ec77096ea0751faa8e9fed6
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:05:52 +0100

    [~] changes in the .h

--- Day10/include/my_opp.h
+++ Day10/include/my_opp.h
@@ -22,4 +22,13 @@ int my_mul(int, int);
 int my_div(int, int);
 int my_mod(int, int);
 int my_usage(int, int);
+
+static const struct operator OPERATORS_FUNCS[] = {
+    {"+", my_add},
+    {"-", my_sub},
+    {"/", my_div},
+    {"*", my_mul},
+    {"%", my_mod},
+    {"", my_usage}
+};
 #endif
--- Day10/my_advanced_do_op/main.c
+++ Day10/my_advanced_do_op/main.c
@@ -7,15 +7,6 @@
 
 #include "../include/my.h"
 
-const struct operator OPERATORS_FUNCS[] = {
-    {"+", &my_add},
-    {"-", &my_sub},
-    {"/", &my_div},
-    {"*", &my_mul},
-    {"%", &my_mod},
-    {"", &my_usage}
-};
-
 int my_usage(int a, int b)
 {
     int useless = (a - a == 0 && b - b == 0);
