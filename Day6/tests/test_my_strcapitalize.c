3dc2789dd63b40d49af7577ac6dbbaacede31add
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:57:37 +0100

    [~] change.

--- Day6/tests/test_my_strcapitalize.c
+++ Day6/tests/test_my_strcapitalize.c
@@ -12,9 +12,10 @@ Test(my_strcapitalize, capitalize_everyword)
     char str[] = "how are you?";
     char str1[] = "how are-you?";
     char str2[] = "how43are[you?";
-    char str3[] = "Hellàhow43are[you?";
+    char str3[] = "heLLO, wOKrl! hOw are YOU?";
 
     cr_assert_str_eq(my_strcapitalize(str), "How Are You?");
     cr_assert_str_eq(my_strcapitalize(str1), "How Are-You?");
     cr_assert_str_eq(my_strcapitalize(str2), "How43are[You?");
+    cr_assert_str_eq(my_strcapitalize(str3), "Hello, Worl! How Are Y?");
 }
