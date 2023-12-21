9b23de447f1305c545a8c23569b2f5a3c1094aef
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:49:39 +0100

    [~] Update.

---
+++ Day5/count_valid_queens_placements.c
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 05
+** File description:
+** Placeholder
+*/
+
+#include "my.h"
+
+static char is_valid(long *queens, int qount, int n, long qurrent)
+{
+    int x = qurrent % n;
+    int y = qurrent / n;
+    int tmp_x;
+    int tmp_y;
+
+    for (int i = 0; i < qount; i++) {
+        tmp_x = queens[i] % n;
+        tmp_y = queens[i] / n;
+        if (tmp_x == x)
+            return 0;
+        if (tmp_y == y)
+            return 0;
+        if (tmp_x - x == tmp_y - y)
+            return 0;
+        if (tmp_x - x == y - tmp_y)
+            return 0;
+    }
+    return 1;
+}
+
+static int my_recursif(long *queens, int qount, int n)
+{
+    int ret = 0;
+    long i = queens[qount - (qount != 0)] * (qount != 0);
+
+    for (; i < n * n; i++) {
+        if (is_valid(queens, qount, n, i) == 0)
+            continue;
+        queens[qount] = i;
+        if (qount + 1 == n)
+            ret++;
+        else
+            ret += my_recursif(queens, qount + 1, n);
+    }
+    return ret;
+}
+
+int count_valid_queens_placements(int n)
+{
+    long queens[n];
+
+    return my_recursif(queens, 0, n);
+}
