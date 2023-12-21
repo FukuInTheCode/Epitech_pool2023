86d10ad40b3f1d1ffe0f77bb2ecce70efdcc92a4
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:20:12 +0100

    [~] update s_format.c -> fix bug 'flgs->precision'

--- my_printf/formats/s_format.c
+++ my_printf/formats/s_format.c
@@ -11,10 +11,10 @@ int s_format_f(char ** buffer, va_list args, my_flgs_t *flgs)
 {
     char *tmp = va_arg(args, char *);
 
-    if (flgs->precision == -1 || presicion > my_strlen(tmp)) {
+    if (flgs->precision == -1 || flgs->precision > my_strlen(tmp)) {
        add_buffer(buffer, tmp, my_strlen(tmp));
     } else {
-        add_buffer(buffer, tmp, precision);
+        add_buffer(buffer, tmp, flgs->precision);
     }
     return (0);
 }
