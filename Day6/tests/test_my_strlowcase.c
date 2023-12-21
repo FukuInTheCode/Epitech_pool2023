bed1303b5c5c9ce3849e7c8f2299e7633b949759
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:57:33 +0100

    [~] ADD SOME TESTS.

--- Day6/tests/test_my_strlowcase.c
+++ Day6/tests/test_my_strlowcase.c
@@ -10,6 +10,35 @@
 Test(my_strlowcase, to_lower_case)
 {
     char str[] = "Hel-lo";
-
     cr_assert_str_eq(my_strlowcase(str), "hel-lo");
+}
+
+Test(my_strlowcase, empty_string)
+{
+    char str[] = "";
+    cr_assert_str_empty(my_strlowcase(str));
+}
+
+Test(my_strlowcase, all_upper_case)
+{
+    char str[] = "HELLO";
+    cr_assert_str_eq(my_strlowcase(str), "hello");
+}
+
+Test(my_strlowcase, all_lower_case)
+{
+    char str[] = "hello";
+    cr_assert_str_eq(my_strlowcase(str), "hello");
+}
+
+Test(my_strlowcase, mixed_case)
+{
+    char str[] = "HeLlO123";
+    cr_assert_str_eq(my_strlowcase(str), "hello123");
+}
+
+Test(my_strlowcase, special_characters)
+{
+    char str[] = "!@#HeLlO12";
+    cr_assert_str_eq(my_strlowcase(str), "!@#hello12");
 }
--- Day6/tests/test_my_strupcase.c
+++ Day6/tests/test_my_strupcase.c
@@ -10,6 +10,35 @@
 Test(my_strupcase, change_char_to_upper_case)
 {
     char str[6] = "Hel-lo";
-
     cr_assert_str_eq(my_strupcase(str), "HEL-LO");
+}
+
+Test(my_strupcase, empty_string)
+{
+    char str[1] = "";
+    cr_assert_str_empty(my_strupcase(str));
+}
+
+Test(my_strupcase, all_upper_case)
+{
+    char str[6] = "HELLO";
+    cr_assert_str_eq(my_strupcase(str), "HELLO");
+}
+
+Test(my_strupcase, all_lower_case)
+{
+    char str[6] = "hello";
+    cr_assert_str_eq(my_strupcase(str), "HELLO");
+}
+
+Test(my_strupcase, mixed_case)
+{
+    char str[10] = "HeLlO123";
+    cr_assert_str_eq(my_strupcase(str), "HELLO123");
+}
+
+Test(my_strupcase, special_characters)
+{
+    char str[12] = "!@#HeLlO12";
+    cr_assert_str_eq(my_strupcase(str), "!@#HELLO12");
 }
