313060d319f076f822727f2be7588971b0fe1246
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:56:56 +0100

    [~] bug fix.

--- Day6/my_strncpy.c
+++ Day6/my_strncpy.c
@@ -11,8 +11,9 @@ char *my_strncpy(char *dest, char const *src, int n)
 {
     int i = 0;
 
-    for (; i < n && src[i] != '\0'; i++)
+    for (; i < n && dest[i] != '\0'; i++)
         dest[i] = src[i];
-    dest[i] = '\0';
+    if (i < n)
+        dest[i] = '\0';
     return dest;
 }
