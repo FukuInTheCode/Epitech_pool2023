1431292ae4244cdf69fccd368bd2651df19450b5
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:19:17 +0100

    [+] add my_put_nbr with modifications for myprintf -> my_put_nbr no longer displays numbers but adds them to the buffer.

---
+++ my_printf/formats/di_format.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my_printf di format
+** File description:
+** di
+*/
+
+#include "../include/my.h"
+
+int di_format_f(char **buffer, va_list args, my_flags_t *flgs)
+{
+
+}
---
+++ my_printf/my_put_nbr.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** my_put_nbr.c
+** File description:
+** new my_put_nbr for myprintf
+*/
+
+char *my_put_nbr(int nb)
+{
+    char *buffer = malloc(1);
+    char tmp;
+
+    buffer[0] = 0;
+
+    for (; nb != 0 ; nb = nb / 10) {
+        tmp = (nb % 10) + 48;
+        add_buffer(&buffer, &tmp, 1);
+    }
+    my_revstr(buffer);
+    return(buffer);
+}
