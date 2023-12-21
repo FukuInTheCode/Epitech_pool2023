ef586866d4078cbbf3cf5a799a89d6905a9a255b
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:30:08 +0100

    [~] do not accept * as a corner character now

--- finalstumper/rush1_2.c
+++ finalstumper/rush1_2.c
@@ -17,7 +17,7 @@ static int verrify_2(int size[2], int x, int y, char c)
     if ((x == 0 || x == l - 1 || y == 0 || y == h - 1) && c == '*'){
         if (x == 0 && (y == 0 || y == h - 1))
             return 1;
-        if (x == l - 1 && (y == 0 || y == h - 1)
+        if (x == l - 1 && (y == 0 || y == h - 1))
             return 1;
         return 0;
     }
