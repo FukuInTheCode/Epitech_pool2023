30cc03c00b9390796a2dd2caf5559a50a1880327
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:20:46 +0100

    [~] fix bug -> delete 'unsigned'

--- my_printf/formats/u_format.c
+++ my_printf/formats/u_format.c
@@ -11,7 +11,7 @@ int u_format_f(char **buffer, va_list args, my_flags_t *flgs)
 {
     unsigned int nbr = va_arg(args, unsigned int);
     char *buf_u = my_put_unbr(nbr);
-    unsigned int tmp = my_strlen(buf_u);
+    int tmp = my_strlen(buf_u);
 
     if (nbr == 0 && flgs->precision == 0)
         return (0);
@@ -22,7 +22,7 @@ int u_format_f(char **buffer, va_list args, my_flags_t *flgs)
     }
     if ((tmp < flgs->width) &&
         (flgs->has_zero == 1) && (flgs->has_minus == 0)) {
-        for (int i = (nbr < 0 || flgs->has_plus); i < flgs->width - tmp; i++)
+        for (int i = (nbr < 0 ); i < flgs->width - tmp; i++)
             add_buffer(&buf_u, " ", 1);
     }
     my_revstr(buf_u);
