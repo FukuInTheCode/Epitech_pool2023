7a32c3a74d57435ee7be65b439befc3e10fd1a43
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:29:18 +0100

    [~] added the is_correct and verify functions to verrify if it is in fact a rush 1 1 square

--- finalstumper/rush1_1.c
+++ finalstumper/rush1_1.c
@@ -7,10 +7,38 @@
 
 #include "include/rush3.h"
 
+static int verrify_1(int size[2], int x, int y, char c)
+{
+    int l = size[0];
+    int h = size[1];
 
+    if (x == 0 || x == l - 1 && y == 0 || y == h - 1 && c == 'o')
+        return 0;
+    if (x == 0 || x == l - 1 && c == '|')
+        return 0;
+    if (y == 0 || y == h - 1 && c == '-')
+        return 0;
+    if (x != 0 && x != l - 1 && y != 0 && y != h - 1 && c == ' ')
+        return 0;
+    return 1;
+}
 
+static int is_correct_1(char *str, int size[2])
+{
+    int l = size[0];
+    int h = size[1];
+    int error = 0;
 
-int rush1_1(char **buffer, char *input, int size[3])
+    error += verrify(size[2], 0, 0, str[0]);
+    for (int y = 0; y != h; y++)
+        for (int x = 1; x != l; x++)
+            error += verrify(size, x, y, str[x + len * y]);
+    if (error != 0)
+        return 84;
+    return 0;
+}
+
+int rush1_1(char **buffer, char *input, int size[2])
 {
     char *name = "[rush1-1] ";
     char *size_str[] = {
@@ -18,6 +46,8 @@ int rush1_1(char **buffer, char *input, int size[3])
         my_put_nbr(size[1])
     };
 
+    if (is_corect_1(input, size) == 84)
+        return 0;
     add_buffer(buffer, name, my_strlen(name));
     add_buffer(buffer, size_str[0], my_strlen(size_str[0]));
     add_buffer(buffer, " ", 1);
