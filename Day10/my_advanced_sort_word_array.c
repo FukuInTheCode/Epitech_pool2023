dd1702e4e2a116c054fd5505c0fec4488b58cc5a
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:05:16 +0100

    [+] Add my_advanced_sort_word_array.c

---
+++ Day10/my_advanced_sort_word_array.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 10
+** File description:
+** Placeholder
+*/
+
+#include "include/my.h"
+
+static void my_cmp(char **tab, int i1, int i2, int cmp)
+{
+    char *tmp = NULL;
+
+    if (cmp != 1)
+        return;
+    tmp = tab[i1];
+    tab[i1] = tab[i2];
+    tab[i2] = tmp;
+}
+
+int my_advanced_sort_word_array(char **tab, int(*cmp)(char const *, char const *))
+{
+    for (int i = 0; tab[i]; i++) {
+        for (int j = 1; tab[j]; j++) {
+            my_cmp(tab, j - 1, j, cmp(tab[j - 1], tab[j]));
+        }
+    }
+}
