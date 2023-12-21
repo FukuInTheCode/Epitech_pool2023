6694e6b52c7c2287ec18efe8f66b0602aa3b19bd
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:06:45 +0100

    [+] Add my_find_node.c

--- Day11/include/my.h
+++ Day11/include/my.h
@@ -73,4 +73,6 @@ void my_rev_list(linked_list_t **);
 int my_apply_on_nodes(linked_list_t *, int(*f)(void *));
 int my_apply_on_matching_nodes(linked_list_t *, int(*f)(),
     void const *data_ref, int(*cmp)());
+linked_list_t *my_find_node(linked_list_t const *, void const *,
+    int(*cmp)());
 #endif
---
+++ Day11/my_find_node.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 11
+** File description:
+** Placeholder
+*/
+
+#include "include/my.h"
+
+linked_list_t *my_find_node(linked_list_t const *begin, void const *data_ref,
+    int (*cmp)())
+{
+    linked_list_t *tmp = begin->next;
+
+    for (; tmp && cmp(tmp->data, data_ref); tmp = tmp->next);
+    return tmp;
+}
