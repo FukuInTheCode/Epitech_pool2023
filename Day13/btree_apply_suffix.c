33cf468d15542fae3dd256bf082ff2b208be025b
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:10:04 +0100

    [~] Update.

---
+++ Day13/btree_apply_suffix.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 13
+** File description:
+** Placeholder
+*/
+
+#include "my.h"
+
+void btree_apply_prefix(btree_t *root, int(*applyf)(void *))
+{
+    if (!root)
+        return;
+    btree_apply_prefix(root->left, applyf);
+    btree_apply_prefix(root->right, applyf);
+    apply(root->data);
+}
