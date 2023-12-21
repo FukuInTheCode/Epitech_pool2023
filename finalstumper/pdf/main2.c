000d035e434e6277e58b043271ff0efede142f18
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:29:56 +0100

    [~] Update.

--- finalstumper/pdf/main2.c
+++ finalstumper/pdf/main2.c
@@ -1,11 +1,10 @@
 int main(void)
 {
-    printf("ASDFZBEFSDQSC");
 //    printf("ABC\n");
-//    printf("ABBBC\n");
-//    printf("B  2B\n");
-//    printf("B   B\n");
-//    printf("B   B\n");
-//    printf("CBBBV\n");
+    printf("ABBBC\n");
+    printf("B   B\n");
+    printf("B   B\n");
+    printf("B   B\n");
+    printf("ABBBB\n");
     return 0;
 }
