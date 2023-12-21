64c665505cd1ce97abed173cbdde1fc6dfea27f7
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:57:45 +0100

    [+] Add unitest.

--- Day6/tests/test_my_showstr.c
+++ Day6/tests/test_my_showstr.c
@@ -12,3 +12,11 @@ Test(my_showstr, print_str_with_unpritable_char, .init = cr_redirect_stdout)
     my_showstr("I like \n ponies!");
     cr_assert_stdout_eq_str("I like \\0a ponies!");
 }
+
+Test(my_showstr, print_str_with_unpritable_char2, .init = cr_redirect_stdout)
+{
+    char str[] = "HelloWorld!";
+    str[3] = 27;
+    my_showstr(str);
+    cr_assert_stdout_eq_str("Hel\\1boWorld!");
+}
