8b8023a232956c78694a3c551365545f41067d59
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:57:26 +0100

    update

---
+++ Day6/tests/test_my_showmem.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 06
+** File description:
+** Placeholder
+*/
+
+#include "../my.h"
+
+Test(my_showmem, ram_infos)
+{
+    char str[] = "Hello je suis une merde totale";
+    my_showmem(str, sizeof(str));
+}
