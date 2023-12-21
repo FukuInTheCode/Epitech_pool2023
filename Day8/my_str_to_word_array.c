d206820b3d44edf0e595d91968edb635eeaa2893
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:02:43 +0100

    [~] Bug fix my_str_to_word_array.c

--- Day8/my_str_to_word_array.c
+++ Day8/my_str_to_word_array.c
@@ -23,6 +23,17 @@ static void init_tmp(size_t count, char ***ret, char **tmp, char const *str)
         (*tmp)[i] -= (*tmp)[i] * (is_alphanum((*tmp)[i]) == 0);
 }
 
+static size_t fill_arr(char const *str, char *tmp, char **ret, size_t sub_i)
+{
+    for (int i = 0; i < my_strlen(str); i++) {
+        if (tmp[i] != '\0' || (tmp[i] == 0 && tmp[i + 1] == 0))
+            continue;
+        ret[sub_i] = malloc(my_strlen(tmp + i + 1));
+        my_strcpy(ret[sub_i++], tmp + i + 1);
+    }
+    return sub_i;
+}
+
 char **my_str_to_word_array(char const *str)
 {
     char **ret = NULL;
@@ -30,17 +41,15 @@ char **my_str_to_word_array(char const *str)
     char *tmp = NULL;
     size_t sub_i = 0;
 
-    for (int i = 0; str[i]; i++)
+    for (int i = 0; str[i]; i++) {
+        if (is_alphanum(str[i - 1]) == 0 && i != 0)
+            continue;
         count += is_alphanum(str[i]) == 0;
+    }
     init_tmp(count, &ret, &tmp, str);
     ret[sub_i] = malloc(my_strlen(tmp) + 1);
     my_strcpy(ret[sub_i++], tmp);
-    for (int i = 0; i < my_strlen(str); i++) {
-        if (tmp[i] != '\0')
-            continue;
-        ret[sub_i] = malloc(my_strlen(tmp + i + 1));
-        my_strcpy(ret[sub_i++], tmp + i + 1);
-    }
+    fill_arr(str, tmp, ret, sub_i);
     ret[sub_i] = NULL;
     free(tmp);
     return ret;
--- Day8/tests/test_my_str_to_word_array.c
+++ Day8/tests/test_my_str_to_word_array.c
@@ -16,7 +16,21 @@ Test(my_str_to_word_array, convert_str_arr_word)
     for (int i = 0; output[i]; i++)
         cr_assert_str_eq(output[i], expected[i]);
     for (int i = 0; output[i]; i++) {
-        my_put_nbr(i);
+        free(output[i]);
+    }
+    free(output);
+}
+
+
+Test(my_str_to_word_array, with_nchar_that_are_next)
+{
+    char *input = "Hello\nWorld\nYouGood\n\nPROUT";
+    char **output = my_str_to_word_array(input);
+    char *expected[] = {"Hello", "World", "YouGood", "PROUT"};
+
+    for (int i = 0; output[i]; i++)
+        cr_assert_str_eq(output[i], expected[i]);
+    for (int i = 0; output[i]; i++) {
         free(output[i]);
     }
     free(output);
