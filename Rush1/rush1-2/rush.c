2dcee0b37230653cb52ece1b5c091a90d8908535
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:13:44 +0100

    [+] Re add folders rush1-? .

---
+++ Rush1/rush1-2/rush.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day Rush 1
+** File description:
+** Placeholder
+*/
+
+#include "my.h"
+
+int rush(int x, int y)
+{
+    char *draw_var = "**/\\/\\";
+
+    if (x <= 0 || y <= 0) {
+        write(2, "Invalid size\n", 13);
+        return 0;
+    }
+    return draw(x, y, draw_var);
+}
