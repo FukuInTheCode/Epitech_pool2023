ae6eeea777ffeb4f30fdc3dde4cd02fe59d525c8
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:01:20 +0100

    [+] Add task04.

---
+++ Day7/task04/main.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 07
+** File description:
+** Placeholder
+*/
+
+#include "../include/my.h"
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++) {
+        for (int j = 0; argv[i][j]; j++)
+            write(1, argv[i] + j, 1);
+        write(1, "\n", 1);
+    }
+}
