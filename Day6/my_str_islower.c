22ba80335d2a7834e404acad5b407e417a0cf46c
Tom C. <tom.carlier@epitech.eu>
2023-12-21 18:56:52 +0100

    [~] bug fix my_str_isalpha.c

--- Day6/my_str_islower.c
+++ Day6/my_str_islower.c
@@ -10,6 +10,8 @@
 
 int my_str_islower(char const *str)
 {
+    if (str[0] == '\0')
+        return 0;
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] < 'a' || 'z' > str[i])
             return 0;
