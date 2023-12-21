b15f8f52f5f074b156b6bd25c5edfc34fddddbc4
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:18:13 +0100

    [~] Update.

--- Miniprintf/mini_printf.c
+++ Miniprintf/mini_printf.c
@@ -7,9 +7,27 @@
 
 #include "include/my.h"
 
-static int is_flags(char const *format, int *i)
+static int is_flags(char const *format, int *i, va_list args)
 {
-    if (!my_strncmp())
+    if (*(format + *i) != '%')
+        return write(1, (format + (*i)++), 1);
+    if (my_strncmp(format + *i, "%%", 2)) {
+        *i += 2;
+        return write(1, "%", 1);
+    }
+    if (my_strncmp(format + *i, "%d", 2) || my_strncmp(format + *i, "%i", 2)) {
+        *i += 2;
+        return my_put_nbr((int)va_arg(args, int));
+    }
+    if (my_strncmp(format + *i, "%s", 2)) {
+        *i += 2;
+        return my_putstr((char *)va_arg(args, char *));
+    }
+    if (my_strncmp(format + *i, "%c", 2)) {
+        *i += 2;
+        return write(1, &(char)va_arg(args, char), 1);
+    }
+    return write(1, (format + (*i)++), 1);
 }
 
 int mini_printf(char const *format, ...)
@@ -18,6 +36,7 @@ int mini_printf(char const *format, ...)
     int ret = 0;
 
     va_start(args, format);
-    for (int i = 0; format[i]; ret += is_flags(format, &i));
+    for (int i = 0; format[i]; ret += is_flags(format, &i, args));
+    va_end(args);
     return ret;
 }
--- Miniprintf/my_strncmp.c
+++ Miniprintf/my_strncmp.c
@@ -5,7 +5,7 @@
 ** Placeholder
 */
 
-#include "include/my.h"
+#include "my.h"
 
 int my_strncmp(char const *s1, char const *s2, int n)
 {
