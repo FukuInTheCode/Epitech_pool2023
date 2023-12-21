04c99afefc25997aed731c05ad076e62b18fc747
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:13:41 +0100

    v1

---
+++ Rush1/square1.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-100-LIL-1-1-rush1-ronan.lefeuvre
+** File description:
+** square1.c
+*/
+
+int draw(int x, int y, char *str)
+{
+    my_putchar(str[2]);
+    for (int i = 0; i <= x - 2; i++) {
+        my_putchar(str[0]);
+    }
+    my_putchar(str[2]);
+    my_putchar('\n');
+    for (int i = 0; i <= y - 2; i++) {
+        my_putchar(str[1]);
+        for (int j = 0; i <= x - 2; i++) {
+            my_putchar(' ');
+        }
+        my_putchar(str[1]);
+    }
+    my_putchar('\n');
+    my_putchar(str[2]);
+    for (int i = 0; i <= x - 2; i++) {
+        my_putchar(str[0]);
+    }
+    my_putchar(str[2]);
+    return (1);
+}
