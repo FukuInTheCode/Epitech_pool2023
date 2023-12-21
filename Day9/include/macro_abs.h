fe0ca0214f26306a057ab965f79daeabf976d4b9
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:03:21 +0100

    [

---
+++ Day9/include/macro_abs.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 09
+** File description:
+** Placeholder
+*/
+
+#ifndef MACRO_ABS_H
+    #define MACRO_ABS_H
+    #define ABS(value) value * ((value > 0) - (value < 0))
+#endif
