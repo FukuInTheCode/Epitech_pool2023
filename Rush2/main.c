cdd6b9217821bf71bd40a06bd9b852865d48877c
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:16:16 +0100

    [~] bug fix rush2.c

--- Rush2/main.c
+++ Rush2/main.c
@@ -5,13 +5,13 @@
 ** Placeholder
 */
 
-#include "inlude/my.h"
+#include "include/my.h"
 
 int main(int argc, char *argv[])
 {
     if (argc < 3)
         return 84;
-    for (int i = 0; i != argc ; i++) {
+    for (int i = 2; i != argc ; i++) {
         write(1, argv[i], 1);
         write(1, ":", 1);
         my_put_nbr(rush( argv[1], *(argv[i])));
--- Rush2/rush2.c
+++ Rush2/rush2.c
@@ -5,11 +5,18 @@
 ** rush2
 */
 
+static char to_lower(char a)
+{
+    if ('A' <= a &&  a <= 'Z')
+        return a + 'a' - 'A';
+    return a;
+}
+
 int rush(char *str , char a)
 {
     int j = 0;
 
     for (int i = 0; str[i]; i++)
-        j += (str[i] == a || str[i] == (a + 'A' - 'a');
+        j += (to_lower(str[i]) == to_lower(a));
     return j;
 }
