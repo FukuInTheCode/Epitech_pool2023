923e30263477d49cc9426dc183e3dfdb390f366b
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:07:14 +0100

    [~] Update.

--- Day11/my_apply_on_matching_nodes.c
+++ Day11/my_apply_on_matching_nodes.c
@@ -10,7 +10,10 @@
 int my_apply_on_matching_nodes(linked_list_t *begin, int(*f)(),
     void const *data_ref, int(*cmp)())
 {
-    for (; begin; begin = begin->next)
-        !cmp(begin->data, data_ref) && f(begin->data);
+    for (; begin; begin = begin->next) {
+        if ((*cmp)(begin->data, data_ref) != 0)
+            continue;
+        (*f)(begin->data);
+    }
     return 0;
 }
