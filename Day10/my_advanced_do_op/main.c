bb7d7d37f676008c9407a4789f6cb30c88a2ed7e
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:05:25 +0100

    [+] Add my_advanced_do_op.c

---
+++ Day10/my_advanced_do_op/main.c
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 10
+** File description:
+** Placeholder
+*/
+
+#include "../include/my.h"
+
+const struct operator OPERATORS_FUNCS[] = {
+    {"+", &my_add},
+    {"-", &my_sub},
+    {"/", &my_div},
+    {"*", &my_mul},
+    {"%", &my_mod},
+    {"", &my_usage}
+};
+
+int my_usage(int a, int b)
+{
+    int useless = (a - a == 0 && b - b == 0);
+
+    write(2, "error: only [ ", my_strlen("error: only [ "));
+    for (int i = 0; OPERATORS_FUNCS[i].op; i++) {
+        write(2, OPERATORS_FUNCS[i].op, my_strlen(OPERATORS_FUNCS[i].op));
+        write(2, " ", 1);
+    }
+    write(2, "] are supported", my_strlen("] are supported") * useless);
+    return 84;
+}
+
+
+static op_func find_op(char *op_str)
+{
+    int i = 0;
+    int len = 0;
+
+    for (; OPERATORS_FUNCS[i].op; i++) {
+        len = my_strlen(OPERATORS_FUNCS[i].op);
+        if (my_strncmp(op_str, OPERATORS_FUNCS[i].op, len) != 0)
+            continue;
+        return OPERATORS_FUNCS[i].f;
+    }
+    return OPERATORS_FUNCS[i].f;
+}
+
+static int do_op(char *av[])
+{
+    int n1 = my_getnbr(av[1]);
+    int n2 = my_getnbr(av[3]);
+
+    return find_op(av[2])(n1, n2);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+        return 84;
+    return do_op(argv);
+}
