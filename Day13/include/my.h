e1b1e3e57b75bb105ec8072b12d800756425636d
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:10:00 +0100

    [~] Update.

---
+++ Day13/include/my.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 13
+** File description:
+** Placeholder
+*/
+
+
+#ifndef MY_H
+    #define MY_H
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include "btree.h"
+
+#endif
