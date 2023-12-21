4d61424e1e4310d64bb44e43aa28df93c3f74363
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:28:54 +0100

    [~] added count function (salculate the dimensions of da square)

--- finalstumper/rush3.c
+++ finalstumper/rush3.c
@@ -7,6 +7,22 @@
 
 #include "include/rush3.h"
 
+int* count(char *str, int size[3])
+{
+    int high = 0;
+    int len = 0;
+
+    for (int i = 0; str[i] != '\0'; i++)
+        if (str[i] == '\n')
+            high++;
+    for (int i = 0; str[i] != '\n'; i++)
+        len++;
+    size[0] = len;
+    size[1] = high;
+    size[2] = NULL;
+    return size;
+}
+
 int rush3(char *input)
 {
     char *buffer = malloc(1);
