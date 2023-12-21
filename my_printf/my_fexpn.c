dbbc40304ccb9165a49ff8c5f350fcd8c3d127bd
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:20:34 +0100

    Merge pull request #17 from EpitechPromo2028/Emma_dev
    Emma dev

---
+++ my_printf/my_fexpn.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** my_printf fexpn
+** File description:
+** Placeholder
+*/
+
+#include "include/my.h"
+
+int my_fexpn(double x, int n, double *mantissa)
+{
+    int ret = 0;
+
+    x *= ((x >= 0) - (x < 0));
+    for (; x >= n; x /= n)
+        ret++;
+    for (; x < 1; x *= n)
+        ret--;
+    *mantissa = x;
+    return ret;
+}
