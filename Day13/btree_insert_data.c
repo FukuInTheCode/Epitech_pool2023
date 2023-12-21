44ce6bd8365e17edeef5628718fb4746936f7073
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:10:05 +0100

    [+] add insertion

---
+++ Day13/btree_insert_data.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day 13
+** File description:
+** Placeholder
+*/
+
+ #include "my.h"
+
+void btree_insert_data(btree_t **root, void *item, int(*cmpf)())
+{
+    if (!root)
+        return;
+    if (!(*root)) {
+        *root = btree_create_node(item)
+        return;
+    }
+    if (cmpf(item, root->item) < 0)
+        return btree_insert_data(&((*root)->left), item, cmpf);
+    return btree_insert_data(&((*root)->right), item, cmpf);
+}
