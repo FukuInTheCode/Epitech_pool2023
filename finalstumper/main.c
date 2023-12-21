2b2afa8f07cb0656ebd531051651e7fd446cd243
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:28:55 +0100

    [~] coding style compliant

--- finalstumper/main.c
+++ finalstumper/main.c
@@ -12,7 +12,10 @@ int main(void)
     char buffer[BUFF_SIZE + 1] = {0};
     int len;
 
-    for (; (len = read(0, buffer + my_strlen(buffer), BUFF_SIZE - my_strlen(buffer))) > 0;);
+    for (len = read(0, buffer + my_strlen(buffer),
+        BUFF_SIZE - my_strlen(buffer)); len > 0;
+        len = read(0, buffer + my_strlen(buffer),
+        BUFF_SIZE - my_strlen(buffer)));
     if (len < 0)
         return 84;
     write(1, buffer, my_strlen(buffer));
