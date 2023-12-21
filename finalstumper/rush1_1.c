c183de078908d7a955016ef41c8cf2bd8d4a9fac
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:28:52 +0100

    [+] add the rush1_1.c func

--- finalstumper/rush1_1.c
+++ finalstumper/rush1_1.c
@@ -8,10 +8,17 @@
 #include "include/rush3.h"
 
 
-int rush1_1(char **buffer, int size[2])
+int rush1_1(char **buffer, int size[3])
 {
     char *name = "[rush1-1] ";
+    char *size_str[] = {
+        my_put_nbr(size[0]),
+        my_put_nbr(size[1])
+    };
 
     add_buffer(buffer, name, my_strlen(name));
-    
+    add_buffer(buffer, size_str[0], my_strlen(size_str[0]));
+    add_buffer(buffer, " ", 1);
+    add_buffer(buffer, size_str[1], my_strlen(size_str[1]));
+    return 0;
 }
