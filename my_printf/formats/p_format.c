5587a235dfe6ec430a2bad687766e3216a969336
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:21:51 +0100

    [~] update -> rev buf for add 0x at the beginning of the buf

--- my_printf/formats/p_format.c
+++ my_printf/formats/p_format.c
@@ -12,6 +12,9 @@ int p_format_f(char **buffer, va_list args, my_flags_t *flgs)
     size_t nbr = va_arg(args, size_t);
     char *buf_p = my_putptr_base(nbr, "0123456789abcdef");
 
+    my_revstr(buf_p);
+    add_buffer(&buf_p, "x0", 2);
+    my_revstr(buf_p);
     add_buffer(buffer, buf_p, my_strlen(buf_p));
     free(buf_p);
     return (0);
