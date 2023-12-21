759e4f5dc34a99cef6e6e995453c0f11a63fc040
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:10:10 +0100

    [~] Update.

--- Day13/btree_apply_infix.c
+++ Day13/btree_apply_infix.c
@@ -13,5 +13,5 @@ void btree_apply_infix(btree_t *root, int(*applyf)(void *))
         return;
     btree_apply_infix(root->left, applyf);
     applyf(root->item);
-    btree_apply_infix(root->left, applyf);
+    btree_apply_infix(root->right, applyf);
 }
