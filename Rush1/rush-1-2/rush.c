b4b4cc9884e987318b273753d4d8e5c7e714e193
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:14:34 +0100

    [~] Update.

---
+++ Rush1/rush-1-2/rush.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day Rush 1
+** File description:
+** Placeholder
+*/
+
+#include "../my.h"
+
+int rush(int x, int y)
+{
+    char *draw_var = "**/\\\\/";
+
+    if (x <= 0 || y <= 0) {
+        write(2, "Invalid size\n", 13);
+        return 0;
+    }
+    draw(x, y, draw_var, 0);
+    return 1;
+}
---
+++ Rush1/rush-1-3/rush.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Pool Day Rush 1
+** File description:
+** Placeholder
+*/
+
+#include "../my.h"
+
+int rush(int x, int y)
+{
+    if (x <= 0 || y <= 0) {
+        write(2, "Invalid size\n", 13);
+        return 0;
+    }
+    draw(x, y, "BBAACC", 0);
+    return 1;
+}
