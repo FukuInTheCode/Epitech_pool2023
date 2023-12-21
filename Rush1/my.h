19cac5774eff09c89253ea46b7385aa1844c63d6
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:14:32 +0100

    [~] Update.

--- Rush1/my.h
+++ Rush1/my.h
@@ -13,6 +13,15 @@
     #define MIN_INTEGER -2147483648
     #define GETNBR_CHECK (is_int_stred(str[i]) || is_sign_stred(str[i]))
 
+
+void my_putchar(char c);
+int my_put_nbr(int nb);
+int my_strlen(char const *str);
+int my_putstr(char const *str);
+void my_swap(int *a, int *b);
+int my_compute_square_root(int nb);
+int my_is_prime(int nb);
+
 static inline void my_print_spe_case(int x, int y, char *str)
 {
     for (int i = 0; i < y; i++) {
@@ -58,12 +67,4 @@ static inline void draw(int x, int y, char *str, char do_print_corner)
     print_last(x, y, str);
 }
 
-void my_putchar(char c);
-int my_put_nbr(int nb);
-int my_strlen(char const *str);
-int my_putstr(char const *str);
-void my_swap(int *a, int *b);
-int my_compute_square_root(int nb);
-int my_is_prime(int nb);
-void draw(int x, int y, char *str, char do_print_corner);
 #endif
